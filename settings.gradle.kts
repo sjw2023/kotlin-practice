c24f56d23b02fc2064fe483a3683984b6c332fff
JoowonSuh <sjw2023@gmail.com>
2024-02-16 16:09:48 +0900

    initializing

---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+rootProject.name = "kotlin-practice"
+include("kotlin-practice-array")
+include("kotlin-practice-refercing-from-java")
